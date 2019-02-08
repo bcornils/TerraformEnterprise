194b52535476a97d8c27d1f19b0b1bd54a1c2b1f
bfuller <8461437+bcornils@users.noreply.github.com>
2019-02-08 11:16:02 -0800

    Update consul.tf

--- consul.tf
+++ consul.tf
@@ -2,3 +2,11 @@ module "consul" {
   source = "hashicorp/consul/aws"
   version = "0.1.0"
 }
+  
+  terraform {
+  backend "consul" {
+    address = "demo.consul.io"
+    path    = "getting-started-BETHTESTING"
+    lock    = false
+  }
+}
