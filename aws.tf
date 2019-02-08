170af12d47702a9731a3476f75d2bff204d1d9a8
bfuller <8461437+bcornils@users.noreply.github.com>
2019-02-08 11:41:39 -0800

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -23,3 +23,16 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "consul" {
+  source = "hashicorp/consul/aws"
+
+  num_servers = "3"
+}
+
+output "consul_server_asg_name" {
+  value = "${module.consul.asg_name_servers}"
+}
